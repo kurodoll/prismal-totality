0775e0821ae792e687c4f2711d85169ba1c26bf4
kurodoll <kurodoll@protonmail.ch>
2019-05-22 23:21:31 +1200

    Set up Phaser

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,27 @@
 const socket = io.connect();
+
+$(() => {
+    // ------------------------------------------------------------------------
+    //                                                        Initialize Phaser
+    // ------------------------------------------------------------------------
+    const phaser_config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        scene: [],
+        render: {
+            'pixelArt': true
+        }
+    };
+
+    const game = new Phaser.Game(phaser_config);
+
+
+    // ------------------------------------------------------------------------
+    //                                                     jQuery Browser Stuff
+    // ------------------------------------------------------------------------
+    // If the browser gets resized, update the Phaser canvas to match
+    $(window).on('resize', () => {
+        game.scale.resize(window.innerWidth, window.innerHeight);
+    });
+});
