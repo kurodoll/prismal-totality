2afb5846d6bf6b7503130fcb529c97aedee52c5f
kurodoll <kurodoll@protonmail.ch>
2019-05-30 20:44:43 +1200

    Minor text change

--- Game/CombatManager.py
+++ Game/CombatManager.py
@@ -7,6 +7,6 @@ class CombatManager:
             defender['active'] = False
             defender['updated'] = True
 
-            return 'You defeated ' + defender['components']['name'] + ' (' + str(damage) + ' damage)'  # noqa
+            return 'You defeated ' + defender['components']['name'] + ' [' + str(damage) + ' damage]'  # noqa
         else:
             return 'You attacked ' + defender['components']['name'] + ' for ' + str(damage) + ' damage'  # noqa
